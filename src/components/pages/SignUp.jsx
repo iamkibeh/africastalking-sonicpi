da95b49e203bc95942df7043801b6bf6057fde02
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-05-25 10:06:20 +0300

    handle user auth with firebase and store user data on firestore

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -9,8 +9,69 @@ import {
 import { Button, Grid, Link } from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Copyright from '../UI/Copyright'
+import { useForm } from 'react-hook-form'
+import { auth } from '../../firebase/firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
 
 const SignUp = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm()
+
+  const onFormSubmit = (data) => {
+    // console.log(data)
+    // createUserWithEmailAndPassword(auth, data.email, data.password)
+    //   .then((userCredential) => {
+    //     console.log(userCredential)
+    //     // create a user in your firebase realtime database too and store the user's data in it
+    //     const user = userCredential.user
+    //     db.collection('users').doc(user.uid).set({
+    //       username: data.username,
+    //       phone_number: data.phone_number,
+    //     })
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+    signUpWithEmailAndPassword(
+      data.email,
+      data.password,
+      data.username,
+      data.phone_number
+    )
+  }
+
+  async function signUpWithEmailAndPassword(
+    email,
+    password,
+    username,
+    phoneNumber
+  ) {
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      const db = getFirestore()
+      const usersCollection = collection(db, 'users')
+
+      await addDoc(usersCollection, {
+        userId: user.uid,
+        email,
+        username,
+        phoneNumber,
+      })
+
+      console.log('User has been signed up!')
+    } catch (error) {
+      console.log('Error signing up user: ', error)
+    }
+  }
   return (
     <div>
       <Container component='main' maxWidth='xs'>
@@ -31,9 +92,9 @@ const SignUp = () => {
           </Typography>
           <Box
             component='form'
-            // onSubmit={handleSubmit}
             noValidate
             sx={{ mt: 1 }}
+            onSubmit={handleSubmit(onFormSubmit)}
           >
             <TextField
               margin='normal'
@@ -44,7 +105,15 @@ const SignUp = () => {
               name='username'
               autoComplete='username'
               autoFocus
+              {...register('username', {
+                required: 'Username is required',
+              })}
             />
+            {errors.username && (
+              <div className='text-red-500 text-xs italic'>
+                {errors.username.message}
+              </div>
+            )}
             <TextField
               margin='normal'
               required
@@ -55,7 +124,19 @@ const SignUp = () => {
               type='email'
               autoComplete='email'
               autoFocus
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: 'Entered value does not match email format',
+                },
+              })}
             />
+            {errors.email && (
+              <div className='text-red-500 text-xs italic'>
+                {errors.email.message}
+              </div>
+            )}
             <TextField
               margin='normal'
               required
@@ -65,7 +146,19 @@ const SignUp = () => {
               type='text'
               id='phone-number'
               autoComplete='phone-number'
+              {...register('phone_number', {
+                required: 'Phone number is required',
+                pattern: {
+                  value: /^[+]\d{12}$/,
+                  message: 'Entered value does not match phone number format',
+                },
+              })}
             />
+            {errors.phone_number && (
+              <div className='text-red-500 text-xs italic'>
+                {errors.phone_number.message}
+              </div>
+            )}
             <TextField
               margin='normal'
               required
@@ -75,7 +168,19 @@ const SignUp = () => {
               type='password'
               id='password'
               autoComplete='current-password'
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 6,
+                  message: 'Password must have at least 6 characters',
+                },
+              })}
             />
+            {errors.password && (
+              <div className='text-red-500 text-xs italic'>
+                {errors.password.message}
+              </div>
+            )}
 
             <Button
               type='submit'
